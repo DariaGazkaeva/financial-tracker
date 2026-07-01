b6a6781ee49689fa918133a216dcbb9f09434be0
DariaGazkaeva <d.gazkaeva@gmail.com>
2026-07-01 17:46:03 +0500

    Add Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import './App.css'
-
 function App() {
   return (
     <>
-      <h1 className='header'>
+      <h1 className='text-4xl md:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-purple-800'>
         Финансовый трекер
       </h1>
     </>
