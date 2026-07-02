7e7c3500bf827a75dcc71b8494f1f4851c51a63f
DariaGazkaeva <d.gazkaeva@gmail.com>
2026-07-02 19:53:36 +0500

    Add AppInput (and clsx)

---
+++ src/common/ui/app-input/AppInput.jsx
@@ -0,0 +1,49 @@
+import './app-input.css';
+import clsx from 'clsx';
+
+function AppInput({
+    label,
+    name,
+    type = 'text',
+    placeholder,
+    value,
+    onChange,
+    error,
+    disabled = false,
+    register,
+    className = '',
+}) {
+    return (
+        <div className='app-input'>
+            {label && (
+                <label
+                    htmlFor={ name }
+                    className="app-input__label"
+                >
+                    { label }
+                </label>
+            )}
+            <input
+                name={ name }
+                className={clsx(
+                    'app-input__field',
+                    error && 'app-input__field--error',
+                    disabled && 'app-input__field--disabled',
+                    className,
+                )}
+                type={ type }
+                placeholder={ placeholder }
+                value={ value }
+                onChange={ onChange }
+                disabled={ disabled }
+            />
+            {error && (
+                <span className="app-input__error">
+                    { error }
+                </span>
+            )}
+        </div>
+    )
+}
+
+export default AppInput
