39ae31b79a3a3c300bfe5dc0119c8e16c3fbb158
DariaGazkaeva <d.gazkaeva@gmail.com>
2026-07-03 14:48:09 +0500

    Add TransactionItem

---
+++ src/components/dashboard/TransactionItem.jsx
@@ -0,0 +1,46 @@
+import clsx from 'clsx';
+
+import './transaction-item.css';
+
+function TransactionItem({
+    transaction,
+    onDelete,
+}) {
+    const {
+        id,
+        description,
+        amount,
+        category,
+        date,
+    } = transaction;
+
+    const isIncome = amount > 0;
+    const formattedAmount = (isIncome ? '+' : '-') + amount;
+
+    const amountClasses = clsx(
+        'transaction-item__amount',
+        isIncome ? 'transaction-item__amount--income' : 'transaction-item__amount--expense',
+    );
+
+    return (
+        <li className="transaction-item">
+            <div className="transaction-item__info transaction-item__info--col">
+                <span className="transaction-item__category">{ category }</span>
+                <span className="transaction-item__description">{ description }</span>
+            </div>
+
+            <div className="transaction-item__info transaction-item__info--row">
+                <span className={ amountClasses }>{ formattedAmount }</span>
+                <span className="transaction-item__date">{ date }</span>
+                <button
+                    className="transaction-item__delete-btn"
+                    onClick={() => onDelete(id)}
+                    aria-label="Удалить транзакцию"
+                >
+                    ✕
+                </button>
+            </div>
+        </li>
+    );
+}
+export default TransactionItem;
