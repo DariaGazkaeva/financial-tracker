a11529761aa431249aef287cc294624eb1ffd853
DariaGazkaeva <d.gazkaeva@gmail.com>
2026-07-03 15:18:01 +0500

    Add TransactionList

---
+++ src/components/dashboard/TransactionList.jsx
@@ -0,0 +1,30 @@
+import TransactionItem from './TransactionItem.jsx';
+
+import './transaction-list.css';
+
+function TransactionList({
+    transactions = [],
+    onDelete,
+}) {
+    if (transactions.length === 0) {
+        return (
+            <div className="transaction-list">
+                <div className="transaction-list__empty">Нет транзакций</div>
+            </div>
+        );
+    }
+
+    return (
+        <ul className="transaction-list">
+            {transactions.map(transaction => (
+                <TransactionItem
+                    key={ transaction.id }
+                    transaction={ transaction }
+                    onDelete={ onDelete }
+                />
+            ))}
+        </ul>
+    );
+}
+
+export default TransactionList;
