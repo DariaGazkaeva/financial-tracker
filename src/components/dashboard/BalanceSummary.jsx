6dcfea7741bd62377d7cb86e2cbc7a8cd0ad1c8c
DariaGazkaeva <d.gazkaeva@gmail.com>
2026-07-03 15:47:57 +0500

    Add BalanceSummary

---
+++ src/components/dashboard/BalanceSummary.jsx
@@ -0,0 +1,37 @@
+import clsx from 'clsx';
+
+import './balance-summary.css';
+
+function BalanceSummary({
+    total = 0,
+    income = 0,
+    expense = 0,
+}) {
+    return (
+        <div className="balance-summary">
+            <div className="balance-summary__card">
+                <span className="balance-summary__label">Баланс</span>
+                <span className="balance-summary__value balance-summary__value--total">
+                    { total }
+                </span>
+            </div>
+
+            <div className="balance-summary__cards-container">
+                <div className="balance-summary__card">
+                    <span className="balance-summary__label">Доходы</span>
+                    <span className="balance-summary__value balance-summary__value--income">
+                        +{ income }
+                    </span>
+                </div>
+
+                <div className="balance-summary__card">
+                    <span className="balance-summary__label">Расходы</span>
+                    <span className="balance-summary__value balance-summary__value--expense">
+                        -{ expense }
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default BalanceSummary;
