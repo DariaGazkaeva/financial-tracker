bb12cc07a4fda0b2865b3d7160619c4888bdcb5c
DariaGazkaeva <d.gazkaeva@gmail.com>
2026-07-08 16:29:41 +0500

    Add utils types

--- src/utils/date-utils.ts
+++ src/utils/date-utils.ts
@@ -1,14 +1,14 @@
-export const getFirstDayOfMonth = () => {
+export const getFirstDayOfMonth = (): Date => {
     const d = new Date();
     return new Date(d.getFullYear(), d.getMonth(), 1);
 };
 
-export const getLastDayOfMonth = () => {
+export const getLastDayOfMonth = (): Date => {
     const d = new Date();
     return new Date(d.getFullYear(), d.getMonth() + 1, 0);
 };
 
-export const formatDate = (date) => {
+export const formatDate = (date: Date): string => {
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
