c535c79d1ec407b53f01163219daa4f02ff18117
DariaGazkaeva <d.gazkaeva@gmail.com>
2026-07-08 17:59:55 +0500

    Add TransactionFilter types

--- src/components/dashboard/transaction-filter/TransactionFilter.tsx
+++ src/components/dashboard/transaction-filter/TransactionFilter.tsx
@@ -6,9 +6,16 @@ const TYPE_OPTIONS = [
     { value: 'all', label: 'Все' },
     { value: 'income', label: 'Доходы' },
     { value: 'expense', label: 'Расходы' },
-];
+] as const;
 
-function TransactionFilter({ typeValue, onChange }) {
+type TypeValue = typeof TYPE_OPTIONS[number]['value'];
+
+interface TransactionFilterProps {
+    typeValue: TypeValue,
+    onChange: (value: TypeValue) => {},
+}
+
+function TransactionFilter({ typeValue, onChange }: TransactionFilterProps) {
     return (
         <div className="transaction-filter">
             {TYPE_OPTIONS.map(option => (
