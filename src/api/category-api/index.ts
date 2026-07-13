012ca4a2d4ed8f24b56ff4ae6ddd0eac9f7b0c7b
DariaGazkaeva <d.gazkaeva@gmail.com>
2026-07-13 19:35:55 +0500

    Add new mock categories

--- src/api/category-api/index.ts
+++ src/api/category-api/index.ts
@@ -5,11 +5,26 @@ import { ICategory } from '@app-api/category-api/types.ts';
 
 export const getCategories = async (): Promise<IApiResponse<ICategory[]>> => {
     const categories: ICategory[] = [
+        // Расходы
         { id: 1, label: 'Еда', type: 'expense' },
         { id: 2, label: 'Транспорт', type: 'expense' },
         { id: 3, label: 'Развлечения', type: 'expense' },
-        { id: 4, label: 'Зарплата', type: 'income' },
-        { id: 5, label: 'Фриланс', type: 'income' },
+        { id: 4, label: 'Жильё', type: 'expense' },
+        { id: 5, label: 'Здоровье', type: 'expense' },
+        { id: 6, label: 'Образование', type: 'expense' },
+        { id: 7, label: 'Одежда', type: 'expense' },
+        { id: 8, label: 'Путешествия', type: 'expense' },
+        { id: 9, label: 'Техника', type: 'expense' },
+        { id: 10, label: 'Спорт', type: 'expense' },
+        { id: 11, label: 'Красота', type: 'expense' },
+        // Доходы
+        { id: 12, label: 'Зарплата', type: 'income' },
+        { id: 13, label: 'Фриланс', type: 'income' },
+        { id: 14, label: 'Инвестиции', type: 'income' },
+        { id: 15, label: 'Кэшбэк', type: 'income' },
+        { id: 16, label: 'Проценты по вкладам', type: 'income' },
+        { id: 17, label: 'Бонусы', type: 'income' },
+        { id: 18, label: 'Прочее', type: 'expense' },
     ];
 
     return {
