0c706b30abe708ea491d9c7317722c3664e66323
DariaGazkaeva <d.gazkaeva@gmail.com>
2026-07-13 11:04:34 +0500

    Improve AppButton

--- src/common/ui/app-button/AppButton.tsx
+++ src/common/ui/app-button/AppButton.tsx
@@ -1,30 +1,37 @@
 import './app-button.css';
 
 interface IAppButtonProps {
-    text: string;
-    onClick?: React.MouseEventHandler<HTMLButtonElement>;
-    theme?: 'primary' | 'secondary';
-    disabled?: boolean;
-    type?: 'button' | 'submit' | 'reset';
+    text?: string,
+    children?: React.ReactNode,
+    onClick?: React.MouseEventHandler<HTMLButtonElement>,
+    theme?: 'primary' | 'secondary',
+    disabled?: boolean,
+    type?: 'button' | 'submit' | 'reset',
+    className?: string,
+    title?: string,
 }
 
 function AppButton({
     text,
+    children,
     onClick,
     theme = 'primary',
     disabled = false,
     type = 'button',
+    className = '',
+    title,
 }: IAppButtonProps) {
     const baseClass = `app-button--${theme}`;
 
     return (
         <button
-            className={`app-button ${baseClass}`}
+            className={`app-button ${baseClass} ${className}`}
             onClick={onClick}
             disabled={disabled}
             type={type}
+            title={title}
         >
-            { text }
+            { text ?? children }
         </button>
     );
 }
